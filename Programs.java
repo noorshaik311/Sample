2f33a6afffbeae3b6906c9e5c40719735513b6fa
Noor Mohammad Shaik <shaik.nm@genisys-group.com>
2022-03-16 16:28:59 +0530

    Bubble sort

--- Programs.java
+++ Programs.java
@@ -1,46 +1,82 @@
-import java.util.Arrays;
+class GFG
+{
+
+static class Node
+{
+    int data;
+    Node prev;
+    Node next;
+};
  
-class Del {
+static Node insertAtTheBegin( Node start_ref, int data)
+{
+    Node ptr1 = new Node();
+    ptr1.data = data;
+    ptr1.next = start_ref;
+    if (start_ref != null)
+    (start_ref).prev = ptr1;
+    start_ref = ptr1;
+    return start_ref;
+}
  
-    public static int[] removeTheElement(int[] arr, int index)
+static void printList( Node start)
+{
+    Node temp = start;
+    System.out.println();
+    while (temp != null)
     {
+        System.out.print( temp.data + " ");
+        temp = temp.next;
+    }
+}
  
-        if (arr == null || index < 0
-            || index >= arr.length) {
+static Node bubbleSort( Node start)
+{
+    int swapped, i;
+    Node ptr1;
+    Node lptr = null;
  
-            return arr;
-        }
-
-        int[] anotherArray = new int[arr.length - 1];
-
-        for (int i = 0, k = 0; i < arr.length; i++) {
+    if (start == null)
+        return null;
  
-            
-            if (i == index) {
-                continue;
+    do
+    {
+        swapped = 0;
+        ptr1 = start;
+ 
+        while (ptr1.next != lptr)
+        {
+            if (ptr1.data > ptr1.next.data)
+            {
+                int t = ptr1.data;
+                ptr1.data = ptr1.next.data;
+                ptr1.next.data = t;
+                swapped = 1;
             }
- 
-            anotherArray[k++] = arr[i];
+            ptr1 = ptr1.next;
         }
- 
-        return anotherArray;
+        lptr = ptr1;
     }
- 
-    public static void main(String[] args)
-    {
- 
-        int[] arr = { 1, 2, 3, 4, 5 };
- 
-        System.out.println("Original Array: "
-                           + Arrays.toString(arr));
- 
-        int index = 2;
+    while (swapped != 0);
+        return start;
+}
 
-        System.out.println("Index to be removed: " + index);
+public static void main(String args[])
+{
+    int arr[] = {12, 56, 2, 11, 1, 90};
+    int list_size, i;
  
-        arr = removeTheElement(arr, index);
+    Node start = null;
  
-        System.out.println("Resultant Array: "
-                           + Arrays.toString(arr));
-    }
+    for (i = 0; i < 6; i++)
+        start=insertAtTheBegin(start, arr[i]);
+ 
+    System.out.println("\n Linked list before sorting ");
+    printList(start);
+ 
+    start = bubbleSort(start);
+
+    System.out.println("\n Linked list after sorting ");
+    printList(start);
+}
 }
